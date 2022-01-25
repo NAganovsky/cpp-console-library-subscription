e0adfd02aa5f76b2fcf4a6735c960eb198b40c57
NAganovsky <boss.focs@mail.ru>
2022-01-25 17:06:00 +0300

    Тест1

--- Native Unit Test Project/UnitTest1.cs
+++ Native Unit Test Project/UnitTest1.cs
@@ -1,15 +1,31 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using main_project;
 
 namespace Native_Unit_Test_Project
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            Assert.Fail();
+
+            [TestMethod]
+            public void TestMethod1()
+            {
+             
+            }
+
+            [TestMethod]
+            public void TestMethod2()
+            {
+                
+            }
+
+            [TestMethod]
+            public void TestMethod3()
+            {
+                
+            }
+
         }
     }
-}
+
